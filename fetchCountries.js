00fa4ddc0f5d7db2737582d69695070f6bb89d33
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-08-15 02:39:07 +0300

    hw12 done
    hw12 done

--- fetchCountries.js
+++ fetchCountries.js
@@ -1,13 +1,7 @@
 // Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
 export default function fetchCountries(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
-  const options = {
-    Headers: {
-      Autorization: '',
-    },
-  };
-
-  return fetch(url, options)
+  return fetch(url)
     .then(resp => resp.json())
-    .then(console.log);
+    .catch(() => errorServerMessage());
 }
